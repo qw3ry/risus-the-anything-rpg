571b5d2f564030c3163b33df812bc2630109ffea
tobias <tobias@eisenreichs.de>
2024-08-31 23:18:51 +0200

    moar fixes

--- module/risus-the-anything-rpg.mjs
+++ module/risus-the-anything-rpg.mjs
@@ -15,7 +15,7 @@ import * as models from './data/_module.mjs';
 
 // Add key classes to the global scope so they can be more easily used
 // by downstream developers
-globalThis.risus-the-anything-rpg = {
+globalThis['risus-the-anything-rpg'] = {
   documents: {
     RisusActor,
     RisusItem,
